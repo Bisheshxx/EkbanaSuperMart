5f249e7152735e9f744bc4f7d1ccbe15a3cd674e
Bisheshxx <bishesh.tuladhar1@gmail.com>
2022-03-25 16:13:12 +0545

    ccs files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
