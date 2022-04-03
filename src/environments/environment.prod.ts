b4abd4001fcba6c93276d9f5d5e234e3cf300784
Bisheshxx <bishesh.tuladhar1@gmail.com>
2022-04-03 11:35:29 +0545

    changes to environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL : 'https://uat.ordering-dalle.ekbana.net/api/v4',
+  auth:'/auth',
+  client_id:'2',
+  client_secret:'olzBb6we0po4B0PSJyDpNGhhSsnvZmeio8sRoASa',
+  grant_type:'password',
+  ApiKey:'q0eq7VRCxJBEW6n1EJkHy4qNLgaS86ztm8DYhGMqerV1eldXa6',
+  WarehouseId:'1'
 };
